90ff6f5df632b05533e38064b7acae11108995cc
eau3 <eau3@illinois.edu>
2019-02-02 14:50:22 -0600

    R script done, now runs using shell

--- run.sh
+++ run.sh
@@ -10,3 +10,6 @@ SMOGON_STATS="https://www.smogon.com/stats/20$YEAR-$MONTH/gen7ou-0.txt"
 
 # download the stats file
 curl $SMOGON_STATS -o Temp/gen7ou-0.txt
+
+# run smogon_data.R
+Rscript smogon_data.R
--- smogon_data.R
+++ smogon_data.R
@@ -1,8 +1,10 @@
+library(jsonlite)
+
 # set vars
 cutoff = 0.01   # only record pokemon with at least cutoff% usage
 
 # read the data we downloaded
-usage_data <- read.table("Temp/gen7ou-0.txt", skip = 5, nrows = 650, header=FALSE, sep="|")
+usage_data <- read.table("Temp/gen7ou-0.txt", skip = 5, nrows = 750, header=FALSE, sep="|", quote="")
 
 # set column names
 colnames(usage_data) <- c("x1", "Rank", "Pokemon", "Usage", "x2", "x3", "x4", "x5", "x6")
@@ -15,4 +17,16 @@ usage_data$x4 <- NULL
 usage_data$x5 <- NULL
 usage_data$x6 <- NULL
 
+# remove the '%' and make a float
+usage_data$Usage <- as.numeric(sub("%", "", usage_data$Usage))
+usage_data <- usage_data[!(usage_data$Usage < cutoff), ]
+
+# returns string w/o leading or trailing whitespace
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+
+usage_data$Pokemon <- trim(usage_data$Pokemon)
+
+# convert to JSON for python
+# smogon_json <- toJSON(usage_data)
+write_json(usage_data, "Temp/smogon.json")
 
